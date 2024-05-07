d5d81c5e02beba144128c820abde67c5c1569431
Rumeysa <michiru.aware.lrn@gmail.com>
2024-05-07 22:27:45 +0300

    Update viewController.swift
    Added annotation in the mapview

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -21,7 +21,26 @@ class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDeleg
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(selectLocation))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer)
     }
+    
+    @objc func selectLocation(gestureRecognizer : UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .began {
+            let nokta = gestureRecognizer.location(in: mapView)
+            let koordinat = mapView.convert(nokta, toCoordinateFrom: mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = koordinat
+            annotation.title = "User Choice"
+            annotation.subtitle = "Subtitle"
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         //print(locations[0].coordinate.latitude)
         //print(locations[0].coordinate.longitude)
