6eb2e9041a5094108c656a893ac97256062cb14d
Rumeysa <michiru.aware.lrn@gmail.com>
2024-05-09 12:35:55 +0300

    Create Listviewcontroller

---
+++ Maps/ListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListViewController.swift
+//  Maps
+//
+//  Created by Aydın Kutlu on 9.05.2024.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
