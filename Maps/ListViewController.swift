f72e44d3f2f2df4450939b6969fd88a84359c81c
Rumeysa <michiru.aware.lrn@gmail.com>
2024-05-09 12:59:53 +0300

    Added tableview functions
    numofsection, cellforRow

--- Maps/ListViewController.swift
+++ Maps/ListViewController.swift
@@ -7,14 +7,25 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
 
     /*
     // MARK: - Navigation
